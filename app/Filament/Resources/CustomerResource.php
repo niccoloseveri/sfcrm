c6727289f8f8a57c1a893a147b7675e6ad53f1a8
niccoloseveri <niccolo.severi@libero.it>
2024-06-26 11:28:17 +0200

    fix3

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -405,7 +405,7 @@ class CustomerResource extends Resource
                                     ->label('Documento')
                                     // This will rename the column to "Download Document" (otherwise, it's just the file name)
                                     ->formatStateUsing(fn () => "Download Documento")
-                                    ->url(fn ($record) =>Storage::url($record->file_path, now()->addHours(1)), true) //Storage::temporaryUrl($record->file_path, now()->addHours(1)), true)
+                                    ->url(fn ($record) =>Storage::url($record->file_path), true) //Storage::temporaryUrl($record->file_path, now()->addHours(1)), true)
                                     // This will make the link look like a "badge" (blue)
                                     ->badge()
                                     ->color(Color::Blue),
